da443051862e6d015c368ee5f44f18ebead54a50
hemant-2 <hemant@softwarehouse.co>
2020-03-01 20:05:17 +0000

    committed initial project

---
+++ src/test/java/com/bis/springboot/training/versioning/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bis.springboot.training.versioning.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
