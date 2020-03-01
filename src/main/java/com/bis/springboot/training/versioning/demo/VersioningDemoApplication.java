92a5d15f1918acaa0532e5e5fb739da1abb3cc62
hemant-2 <hemant@softwarehouse.co>
2020-03-01 20:42:11 +0000

    ALL API Added

--- src/main/java/com/bis/springboot/training/versioning/demo/VersioningDemoApplication.java
+++ src/main/java/com/bis/springboot/training/versioning/demo/VersioningDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class VersioningDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(VersioningDemoApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/bis/springboot/training/versioning/demo/controller/ApiController.java
@@ -0,0 +1,49 @@
+package com.bis.springboot.training.versioning.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping(value = "/api" , produces="application/apiversion.app-v1+json")
+    public String apiWithContentTypeV1() {
+        return "called API with the content type with V1!";
+    }
+
+    @GetMapping(value = "/api" , produces="application/apiversion.app-v2+json")
+    public String apiWithContentTypeV2() {
+        return "called API with the content type V2!";
+    }
+
+    @GetMapping(value = "/api" , headers="version=1")
+    public String apiWithHeadersV1() {
+        return "called API with the apiWithHeadersV1!";
+    }
+
+    @GetMapping(value = "/api" , headers="version=2")
+    public String apiWithHeadersV2() {
+        return "called API with the apiWithHeadersV2!";
+    }
+
+    @GetMapping(value = "/api" , params="version=1")
+    public String apiWithParameterV1() {
+        return "called API with the apiWithParameterV1!";
+    }
+
+    @GetMapping(value = "/api" , params="version=2")
+    public String apiWithParamsV2() {
+        return "called API with the apiWithParamsV2!";
+    }
+
+    @GetMapping(value = "/api/v1")
+    public String apiV1() {
+        return "called API with /v1/api!";
+    }
+
+    @GetMapping("/api/v2")
+    public String apiV2() {
+        return "called API with /v2/api!";
+    }
+
+  }
